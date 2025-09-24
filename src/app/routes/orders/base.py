7ade80cd80f3eb0ef127b8b1dbca4856172339e1
r.tatarinov <r.tatarinov@optimism.ru>
2025-09-24 20:00:34 +0300

    конфигурация связей

--- src/app/routes/orders/base.py
+++ src/app/routes/orders/base.py
@@ -34,9 +34,9 @@ class BaseRouter(
             DetectionResult(
                 order_id=order.id,
                 label=_['label'],
-                confidence=_['confidence'],
-                bbox_norm=_['bbox_norm'],
-                bbox_abs=_['bbox_abs']
+                confidence=str(_['confidence']),
+                bbox_norm=str(_['bbox_norm']),
+                bbox_abs=str(_['bbox_abs'])
             ) for _ in data
         ]
 
