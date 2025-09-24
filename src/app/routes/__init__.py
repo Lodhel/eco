aa0bcb297b429e1381ef4fba277a1689046573b3
r.tatarinov <r.tatarinov@optimism.ru>
2025-09-24 20:21:58 +0300

    добавление ручки для изображений

--- src/app/routes/__init__.py
+++ src/app/routes/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
+from src.app.routes.images.images import image_router
 from src.app.routes.orders.orders import order_router
 
 router = APIRouter()
 
 
 router.include_router(order_router, prefix='/api/v1')
+router.include_router(image_router, prefix='/api/v1')
--- src/app/routes/images/images.py
+++ src/app/routes/images/images.py
@@ -1,6 +1,8 @@
 import os
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Path, HTTPException
 from fastapi_utils.cbv import cbv
+from starlette.requests import Request
+from starlette.responses import Response
 from starlette.responses import FileResponse
 
 from src.app.routes.orders.base import BaseRouter
@@ -21,7 +23,17 @@ class ImageRouter(BaseRouter):
         tags=image_tags,
         description="GET-операция для получения изображения по имени файла"
     )
-    async def get_image(self, filename: str):
+    async def get(
+        self,
+        request: Request,
+        response: Response,
+        filename: str = Path(
+            ...,
+            title="название файла",
+            description="название файла",
+            example="9be15cad5969bcf020d21950673bdb51.jpg"
+        )
+    ):
         file_path = os.path.join(UPLOAD_DIRECTORY, filename)
 
         if not os.path.exists(file_path):
